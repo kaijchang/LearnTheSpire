65ae15ef8adc257bf370571da95c9cd2bab3b21c
Kai Chang <kaijchang@gmail.com>
2018-11-16 16:45:57 -0800

    process implied upgrade changes and sort vocab for consistency

--- card_name_to_vector.py
+++ card_name_to_vector.py
@@ -17,7 +17,31 @@ def get_vocab():
 
     flattened_words = [word for sublist in words for word in sublist]
 
-    return set(flattened_words)
+    return list(sorted(set(flattened_words)))
+
+
+def get_card(card_name, upgraded=False):
+    card = next(filter(lambda card: card_name.casefold() == card['Name'].casefold(), cards))
+
+    cost = card['Cost'] if type(card['Cost']) == int else (
+        int(re.sub(r'[()]', '', card['Cost']).split()[1]) if upgraded else
+        int(re.sub(r'[()]', '', card['Cost']).split()[0]))
+
+    if upgraded and card['Description (Upgraded)']:
+        text = card['Description (Upgraded)']
+
+    else:
+        text = card['Description']
+
+        match = re.search(r'[0-9]+ \([0-9]+\)', text)
+
+        while match:
+            text = text[:match.start()] + \
+                   (re.sub(r'[()]', '', match.group(0)).split()[1] if upgraded else \
+                   re.sub(r'[()]', '', match.group(0)).split()[0]) + text[match.end():]
+            match = re.search(r'[0-9]+ \([0-9]+\)', text)
+
+    return cost, text
 
 
 def card_name_to_vectors(card_name):
@@ -27,7 +51,6 @@ def card_name_to_vectors(card_name):
         card_name = card_name[:-2]
         upgraded = True
 
-    card = next(filter(lambda card: card_name.casefold() == card['Name'].casefold(), cards))
-    card_text = list(map(clean_text, card['Description (Upgraded)'] if upgraded else card['Description'].split()))
+    cost, text = get_card(card_name, upgraded)
 
-    return card['Cost'], [card_text.count(word) for word in get_vocab()]
+    return cost, [text.split().count(word) for word in get_vocab()]
